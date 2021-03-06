ece44a2d8db381cf936af055649a27de4297b2d9
dboisse <dawid.boisse@gmail.com>
2021-03-06 18:15:12 +0100

    Prepare /version endpoint

---
+++ web/src/main/java/mr/cookie/SimpleApp.java
@@ -0,0 +1,14 @@
+package mr.cookie;
+
+import org.jetbrains.annotations.Nullable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApp {
+
+    public static void main(@Nullable String[] args) {
+        SpringApplication.run(SimpleApp.class, args);
+    }
+
+}
--- web/src/main/java/mr/cookie/controllers/VersionController.java
+++ web/src/main/java/mr/cookie/controllers/VersionController.java
@@ -1,8 +1,38 @@
 package mr.cookie.controllers;
 
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+// TODO: junits (5)
+@RequiredArgsConstructor
+@RestController("/version")
 public class VersionController {
+
+    @Value("${spring.application.name:#{null}}")
+    private String appName;
+
+    @Value("${spring.application.version:#{null}}")
+    private String appVersion;
+
+    // TODO: move these to a service class
+    private Map<String, String> appProperties;
+
+    @PostConstruct
+    protected void initAppProperties() {
+        this.appProperties = Map.of(
+            "name", appName,
+            "version", appVersion);
+    }
+
+    @GetMapping
+    public @NotNull Map<String, String> getAppProperties() {
+        // TODO: return a DTO
+        return appProperties;
+    }
 
 }
