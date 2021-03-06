7e76031625457580d29b87533f8320e09ac74421
dboisse <dawid.boisse@gmail.com>
2021-03-06 21:55:48 +0100

    Extracting Service Logic into Separate Module

--- web/src/main/java/mr/cookie/controllers/VersionController.java
+++ web/src/main/java/mr/cookie/controllers/VersionController.java
@@ -5,11 +5,13 @@ import mr.cookie.VersionService;
 import mr.cookie.dto.AppPropertiesDto;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO: junits (5)
 @RequiredArgsConstructor
-@RestController("/version")
+@RestController
+@RequestMapping("/version")
 public class VersionController {
 
     private final @NotNull VersionService versionService;
